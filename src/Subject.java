327f3560468ddfcab0766d4fa86379ce6028ba5a
srengty <sreng.itc@gmail.com>
2025-12-24 08:49:54 +0700

    Add javadoc comments

--- src/Subject.java
+++ src/Subject.java
@@ -1,5 +1,16 @@
+/**
+ * A course in year 2 class at ITC.
+ * @author Sreng
+ * @author Setha
+ */
 public class Subject {
+    /**
+     * Subject name (e.g. {@code "Introduction to SE"})
+     */
     String name;
+    /**
+     * Total teaching hours including lecture, labs, and assignments
+     */
     int totalHours;
     public static void main(String[] args) {
         Subject s = new Subject();
